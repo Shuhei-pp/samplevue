283df5517bd75472a23adb3d71d30c0ca3ea618d
Shuhei-pp <lotakotayota@gmail.com>
2021-09-28 07:48:53 +0900

    seeder作成

--- backend/database/seeders/TaskTableSeeder.php
+++ backend/database/seeders/TaskTableSeeder.php
@@ -5,7 +5,7 @@ namespace Database\Seeders;
 use App\Models\Task;
 use Illuminate\Database\Seeder;
 
-class TasksTableSeeder extends Seeder
+class TaskTableSeeder extends Seeder
 {
     /**
      * Run the database seeds.
